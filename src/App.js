c15bc00a9fc0c20cfe9da65e3446fda7e43b54a5
Mangeshi <thisismangeshi@gmail.com>
2021-03-19 19:03:23 +0530

    structured the folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Home from "./components/Home/Home"
-
+import About from "./components/About/About"
 function App() {
     return (
       <>
       <Home/>
-
+      <About/>
       </>
     )
 }
