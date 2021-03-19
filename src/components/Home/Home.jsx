9fd3d7ddb5da803feeb4a0592a163da3a29e360a
Mangeshi <thisismangeshi@gmail.com>
2021-03-19 18:47:18 +0530

    styled about page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
         <>
             <div className="wrapper">
                 <nav className="navbar sticky-top navbar-light bg-light">
-                    <a className="navbar-brand" href="#">
+                    {/* <a className="navbar-brand" href="#">
                         <ul></ul>
-                    </a>
+                    </a> */}
                 </nav>
                 <div className="container-fluid">
                     <div className="container">
@@ -27,6 +27,25 @@ const Home = () => {
                                 <h6>
                                     I design and develop responsive, optimized
                                     and efficient websites.
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container-fluid aboutWrapper">
+                    <div className="row">
+                        <div className="about text-center col-md-12 col-xs-12">
+                            <div className="aboutcontent">
+                                <h3>About Me</h3>
+                                <h6 >
+                                    I am a self-taught developer and I started
+                                    my journey past 1 year. At the moment I am
+                                    tailoring my skills on HTML, CSS, JavaScript,
+                                    Bootstrap and React. This gives me a lot of
+                                    joy when I see my progress, while I am
+                                    learning and trying to take action on what I
+                                    have learned.
                                 </h6>
                             </div>
                         </div>
