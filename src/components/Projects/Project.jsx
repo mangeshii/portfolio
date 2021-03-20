47faa26154de1e9a1f552199e1483676e09fa1c2
Mangeshi <thisismangeshi@gmail.com>
2021-03-20 15:47:03 +0530

    added focus effect on hovering the card

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -2,8 +2,8 @@ import "./Project.css";
 const Project = () => {
     return (
         <div className="projectWrapper container-fluid">
-            <div className="container projCont">
                 <h1>MyWork</h1>
+            <div className="container projCont">
 
                 <div class="row">
                     <div class="col-sm-4 col-xs-12">
