f3438433c0185776ae22359901fcdab24ff41589
Mangeshi <thisismangeshi@gmail.com>
2021-03-20 14:53:59 +0530

    styled education page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./components/Home/Home"
 import About from "./components/About/About"
 import Education from "./components/Education/Education"
+import Project from "./components/Projects/Project"
 function App() {
     return (
       <>
       <Home/>
       <About/>
       <Education/>
+      <Project/>
       </>
     )
 }
---
+++ src/components/Projects/Project.jsx
@@ -0,0 +1,56 @@
+import "./Project.css";
+const Project = () => {
+    return (
+        <div className="projectWrapper container-fluid">
+            <div className="container projCont">
+                <h1>myWork</h1>
+
+                <div class="row">
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="card">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="card">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="card">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="card">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="card">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-12">
+                        <div class="card">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Project;
