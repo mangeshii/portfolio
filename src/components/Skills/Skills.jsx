982148c0dc72c5cc8e6441c125333bbd2cb30e0a
Mangeshi <thisismangeshi@gmail.com>
2021-03-20 18:46:48 +0530

    styled the icons

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,39 +4,39 @@ const Skills = () => {
     return (
         <>
             <div className="skillWrapper">
-                <div className="skillHeading"></div>
+                <div className="skillHeading">
+                    <h2>Skill Stack</h2>
+                    <h5>Below is the quick overview of my main technical skills sets and tools I use</h5>
+                </div>
                 <div className="container skillStack">
-                    <div className="card col-md-4">
-
-                    <i class="fab fa-html5"></i>
+                    <div className="card col-md-4 col-sm-6">
+                        <i class="fab fa-html5"></i>
                     </div>
-                    <div className="card col-md-4">
-                    <i class="fab fa-css3-alt"></i>
-
-
+                    <div className="card col-md-4 col-sm-6">
+                        <i class="fab fa-css3-alt"></i>
                     </div>
-                    <div className="card col-md-4">
-                    <i class="fab fa-js"></i>
+                    <div className="card col-md-4 col-sm-6">
+                        <i class="fab fa-js"></i>
                     </div>
-                    <div className="card col-md-4">
-                    <i class="fab fa-bootstrap"></i>
+                    <div className="card col-md-4 col-sm-6">
+                        <i class="fab fa-bootstrap"></i>
                     </div>
-                    <div className="card col-md-4">
-                    <i class="fab fa-react"></i>
+                    <div className="card col-md-4 col-sm-6">
+                        <i class="fab fa-react"></i>
                     </div>
-                    <div className="card col-md-4">
-                    <i class="fas fa-database"></i>
+                    <div className="card col-md-4 col-sm-6">
+                        <i class="fas fa-database"></i>
                     </div>
 
-                    <div className="card col-md-4">
-                    <i class="fab fa-sass"></i>
+                    <div className="card col-md-4 col-sm-6">
+                        <i class="fab fa-sass"></i>
                     </div>
-                    <div className="card col-md-4">
-                    <i class="fas fa-terminal"></i>
+                    <div className="card col-md-4 col-sm-6">
+                        <i class="fas fa-terminal"></i>
                     </div>
 
-                    <div className="card col-md-4">
-                    <i class="fab fa-git-alt"></i>
+                    <div className="card col-md-4 col-sm-6">
+                        <i class="fab fa-git-alt"></i>
                     </div>
                 </div>
             </div>
