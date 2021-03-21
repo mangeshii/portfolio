911b7c41d35ffcc2ea28a34ca17e82668effe298
Mangeshi <thisismangeshi@gmail.com>
2021-03-22 01:37:25 +0530

    changed the theme

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,15 +9,15 @@ const Contact = () => {
                     <h3>LET'S CONNECT</h3>
                 </div>
                 <div className="contactIcons">
-                   <a href="https://www.linkedin.com/in/mangeshipatil/"> <i class="fab fa-linkedin-in"></i></a>
-                   <a href="https://github.com/mangeshii"><i class="fab fa-github"></i></a>
-                   <a href="https://twitter.com/mangeshi_patil"><i class="fab fa-twitter"></i></a>
+                   <a href="https://www.linkedin.com/in/mangeshipatil/"> <i class="fab  fabicon fa-linkedin-in"></i></a>
+                   <a href="https://github.com/mangeshii"><i class="fab fabicon fa-github"></i></a>
+                   <a href="https://twitter.com/mangeshi_patil"><i class="fab fabicon fa-twitter"></i></a>
                 </div>
                 <div className="footerContent">
                 <h5>Build with ♥️ Mangeshi </h5>
                 </div>
-                <div className="quote">
-                    <h5>Living, Learning and Leveling up one day at a time</h5>
+                <div className="quote" style={{display:'flex',justifyContent:'center'}}>
+                    <h5 style={{width:'80%',textAlign:'center'}}>Living, Learning and Leveling up one day at a time</h5>
                 </div>
             </div>
             </div>
