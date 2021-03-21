3d78e0d1325502e4a527e6d05abdb35fccb7125b
Mangeshi <thisismangeshi@gmail.com>
2021-03-21 15:51:00 +0530

    made skill page responsive

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -9,33 +9,33 @@ const Skills = () => {
                     <h6>Below is the quick overview of my main technical skills sets and tools I use</h6>
                 </div>
                 <div className="container skillStack">
-                    <div className="card col-md-4 col-sm-6">
+                    <div className="card col-md-4 col-sm-6 col-xs-12">
                         <i class="fab fa-html5"></i>
                     </div>
-                    <div className="card col-md-4 col-sm-6">
+                    <div className="card col-md-4 col-sm-6 col-xs-12">
                         <i class="fab fa-css3-alt"></i>
                     </div>
-                    <div className="card col-md-4 col-sm-6">
+                    <div className="card col-md-4 col-sm-6 col-xs-12">
                         <i class="fab fa-js"></i>
                     </div>
-                    <div className="card col-md-4 col-sm-6">
+                    <div className="card col-md-4 col-sm-6 col-xs-12">
                         <i class="fab fa-bootstrap"></i>
                     </div>
-                    <div className="card col-md-4 col-sm-6">
+                    <div className="card col-md-4 col-sm-6 col-xs-12">
                         <i class="fab fa-react"></i>
                     </div>
-                    <div className="card col-md-4 col-sm-6">
+                    <div className="card col-md-4 col-sm-6 col-xs-12">
                         <i class="fas fa-database"></i>
                     </div>
 
-                    <div className="card col-md-4 col-sm-6">
+                    <div className="card col-md-4 col-sm-6 col-xs-12">
                         <i class="fab fa-sass"></i>
                     </div>
-                    <div className="card col-md-4 col-sm-6">
+                    <div className="card col-md-4 col-sm-6 col-xs-12">
                         <i class="fas fa-terminal"></i>
                     </div>
 
-                    <div className="card col-md-4 col-sm-6">
+                    <div className="card col-md-4 col-sm-6 col-xs-12">
                         <i class="fab fa-git-alt"></i>
                     </div>
                 </div>
