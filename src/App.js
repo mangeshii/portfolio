426c1b5103b7e533114935893c9d3774e46416e6
Mangeshi <thisismangeshi@gmail.com>
2021-03-24 00:49:23 +0530

    made changes in education section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from "./components/Home/Home"
+import About from "./components/About/About"
 import Education from "./components/Education/Education"
 import Project from "./components/Projects/Project"
 import Skills from "./components/Skills/Skills"
@@ -8,6 +9,7 @@ function App() {
     return (
       <>
       <Home/>
+      <About/>
       <Education/>
       <Project/>
       <Skills/>
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -3,53 +3,39 @@ import "./Education.css";
 const Education = () => {
     return (
         <>
-            <section class="section">
-                <div class="sectioncontainer">
-                    <div class="columns">
-                        <div class="column">
-                            <div class="text">
-                                <h1>About me</h1>
-                                <p>
-                                    I am a self-taught developer and I started
-                                    my journey past 1 year. At the moment I am
-                                    tailoring my skills on HTML, CSS,
-                                    JavaScript, Bootstrap and React. This gives
-                                    me a lot of joy when I see my progress,
-                                    while I am learning and trying to take
-                                    action on what I have learned.
-                                </p>
-                            </div>
+            <div className="main">
+                <div className=" educationWrapper text-center" id="education">
+                    <div class="container1">
+                        <div>
+                            <h3 className="edu">Education</h3>
                         </div>
-                        <div class="educationsection">
-                            <div class="isnarrow">
-                                <div class="timeline">
-                                    <div class="timeline-text">
-                                        <h2>B.TECH</h2>
-                                        <p>
-                                            Electronics and Telecommunication,
-                                            Don Bosco Technical Institute
-                                        </p>
-                                        <p>2018-present</p>
-                                    </div>
-                                    <div class="timeline-text">
-                                        <h2>Diploma</h2>
-                                        <p>
-                                            Electronics and Telecommunication,
-                                            St. Xaviers Technical Institute
-                                        </p>
-                                        <p>2015-2018</p>
-                                    </div>
-                                    <div class="timeline-text">
-                                        <h2>SSC</h2>
-                                        <p>Maharashtra State Board</p>
-                                        <p> Till 2015</p>
-                                    </div>
-                                </div>
+                        <div class="timeline">
+                            <div class="timeline-text">
+                                <h2>B.TECH</h2>
+                                <p>
+                                    Electronics and Telecommunication, Don Bosco
+                                    Technical Institute
+                                </p>
+                                <p>2018-present</p>
+                            </div>
+                            <div class="timeline-text">
+                                <h2>Diploma</h2>
+                                <p>
+                                    Electronics and Telecommunication, St.
+                                    Xaviers Technical Institute
+                                </p>
+                                <p>2015-2018</p>
+                            </div>
+                            <div class="timeline-text">
+                                <h2>SSC</h2>
+                                <p>Maharashtra State Board</p>
+                                <p> Till 2015</p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
+            <hr />
         </>
     );
 };
