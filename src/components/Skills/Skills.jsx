80439712efb946b1cf4d9c2961e914ee6ef6b4ca
Mangeshi <thisismangeshi@gmail.com>
2021-03-24 16:30:50 +0530

    made changes in skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,46 +3,50 @@ import "./Skills.css";
 const Skills = () => {
     return (
         <>
-            <div className="skillWrapper">
-                <div className="skillHeading">
-                    <h3>Skill Stack</h3>
-                    <h6>Below is the quick overview of my main technical skills sets and tools I use</h6>
+            <section class="skillsection">
+                <div class="skillHeader">
+                    <div class="headingContainer">
+                        <div class="skillcolumn">
+                            <h1>Skill Stack</h1>
+                            <p>
+                                Below is the quick overview of my main technical
+                                skills sets and tools I use
+                            </p>
+                        </div>
+                        <div class="skillContainer">
+                            <div class="skillStack">
+                                <div class="card col-md-4 col-sm-6 col-xs-12">
+                                    <i class="fab fa-html5"></i>
+                                </div>
+                                <div class="card col-md-4 col-sm-6 col-xs-12">
+                                    <i class="fab fa-css3-alt"></i>
+                                </div>
+                                <div class="card col-md-4 col-sm-6 col-xs-12">
+                                    <i class="fab fa-js"></i>
+                                </div>
+                                <div class="card col-md-4 col-sm-6 col-xs-12">
+                                    <i class="fab fa-bootstrap"></i>
+                                </div>
+                                <div class="card col-md-4 col-sm-6 col-xs-12">
+                                    <i class="fab fa-react"></i>
+                                </div>
+                                <div class="card col-md-4 col-sm-6 col-xs-12">
+                                    <i class="fas fa-database"></i>
+                                </div>
+                                <div class="card col-md-4 col-sm-6 col-xs-12">
+                                    <i class="fab fa-sass"></i>
+                                </div>
+                                <div class="card col-md-4 col-sm-6 col-xs-12">
+                                    <i class="fas fa-terminal"></i>
+                                </div>
+                                <div class="card col-md-4 col-sm-6 col-xs-12">
+                                    <i class="fab fa-git-alt"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="skillcontainer">
-                <div className="container skillStack">
-                    <div className="card col-md-4 col-sm-6 col-xs-12">
-                        <i class="fab fa-html5"></i>
-                    </div>
-                    <div className="card col-md-4 col-sm-6 col-xs-12">
-                        <i class="fab fa-css3-alt"></i>
-                    </div>
-                    <div className="card col-md-4 col-sm-6 col-xs-12">
-                        <i class="fab fa-js"></i>
-                    </div>
-                    <div className="card col-md-4 col-sm-6 col-xs-12">
-                        <i class="fab fa-bootstrap"></i>
-                    </div>
-                    <div className="card col-md-4 col-sm-6 col-xs-12">
-                        <i class="fab fa-react"></i>
-                    </div>
-                    <div className="card col-md-4 col-sm-6 col-xs-12">
-                        <i class="fas fa-database"></i>
-                    </div>
-
-                    <div className="card col-md-4 col-sm-6 col-xs-12">
-                        <i class="fab fa-sass"></i>
-                    </div>
-                    <div className="card col-md-4 col-sm-6 col-xs-12">
-                        <i class="fas fa-terminal"></i>
-                    </div>
-
-                    <div className="card col-md-4 col-sm-6 col-xs-12">
-                        <i class="fab fa-git-alt"></i>
-                    </div>
-                </div>
-            </div>
-            </div>
-            <hr style={{marginTop:'10%'}}/>
+            </section>
         </>
     );
 };
