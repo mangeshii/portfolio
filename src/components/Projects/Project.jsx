868aff37e96361b50d17e416549f8f7f82d43048
Mangeshi <thisismangeshi@gmail.com>
2021-03-30 17:10:18 +0530

    made changes in project section

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -11,181 +11,165 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Project = () => {
     return (
         <>
-            <section className="projectsection" id="project">
-                <div className="projectHeader">
-                    <h1>My Recent Work</h1>
-                    <h6>Here are a few projects I've worked on recently.</h6>
-                </div>
-                <div className="container-lg">
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3">
-                        <div className="col">
-                            <div class="flip-card ">
-                                <div class="flip-card-inner">
-                                    <div class="flip-card-front card1">
-                                        <img
-                                            className="frontcard"
-                                            src={weatherapp}
-                                            alt="Avatar"
-                                        />
-                                        <h3>WEATHER APP</h3>
-                                    </div>
-                                    <div class="flip-card-back">
-                                        <p>
-                                            A Weather App is capable of pulling
-                                            weather information such as
-                                            temperature, weather description,
-                                            city and country name from the API.
-                                        </p>
-                                        <a href="https://github.com/mangeshii/weather-app-using-react">
-                                            <button className="gitbutton">
-                                                See on Github{" "}
-                                            </button>
-                                        </a>
-                                    </div>
+            <section class="project-section" id="project">
+                <div class="project-container">
+                <h1>My Recent Work</h1>
+                    <div class="project-cards">
+                        <div class="flip-card col-lg-4 col-md-6 col-xs-12">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front card1">
+                                    <img
+                                        className="frontcard"
+                                        src={weatherapp}
+                                        alt="Avatar"
+                                    />
+                                    <h3>WEATHER APP</h3>
                                 </div>
-                            </div>
-                        </div>
-
-                        <div className="col">
-                            <div class="flip-card">
-                                <div class="flip-card-inner">
-                                    <div class="flip-card-front card2">
-                                        <img
-                                            className="frontcard"
-                                            src={todo}
-                                            alt="Avatar"
-                                        />
-                                        <h3>TODO APP</h3>
-                                    </div>
-                                    <div class="flip-card-back">
-                                        <p>
-                                            A Todo App is designed which has the
-                                            capability to add, delete and mark
-                                            as complete
-                                        </p>
-
+                                <div class="flip-card-back">
+                                    <p>
+                                        A Weather App is capable of pulling
+                                        weather information such as temperature,
+                                        weather description, city and country
+                                        name from the API.
+                                    </p>
+                                    <a href="https://github.com/mangeshii/weather-app-using-react">
                                         <button className="gitbutton">
                                             See on Github
                                         </button>
-                                    </div>
+                                    </a>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="col">
-                            <div class="flip-card">
-                                <div class="flip-card-inner">
-                                    <div class="flip-card-front card3">
-                                        <img
-                                            className="frontcard"
-                                            src={pokedex}
-                                            alt="Avatar"
-                                        />
-                                        <h3>POKEDEX</h3>
-                                    </div>
-                                    <div class="flip-card-back">
-                                        <p>
-                                            It consists of all the pokemon data
-                                            which is fetched from API. Once you
-                                            click in a particular pokemon card
-                                            it with render a page will more
-                                            information about that pokemon.
-                                        </p>
-                                        <a href="https://github.com/mangeshii/pokedex-using-react">
-                                            <button className="gitbutton">
-                                                See on Github
-                                            </button>
-                                        </a>
-                                    </div>
+                        <div class="flip-card col-lg-4 col-md-6 col-xs-12">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front card2">
+                                    <img
+                                        className="frontcard"
+                                        src={todo}
+                                        alt="Avatar"
+                                    />
+                                    <h3>TODO APP</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <p>
+                                        A Todo App is designed which has the
+                                        capability to add, delete and mark as
+                                        complete
+                                    </p>
+
+                                    <button className="gitbutton">
+                                        See on Github
+                                    </button>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="col">
-                            <div class="flip-card">
-                                <div class="flip-card-inner">
-                                    <div class="flip-card-front card4">
-                                        <img
-                                            className="frontcard"
-                                            src={portfolio}
-                                            alt="Avatar"
-                                        />
-                                        <h3>PORTFOLIO</h3>
-                                    </div>
-                                    <div class="flip-card-back">
-                                        <p>
-                                            It is a responsive website made with
-                                            the help of html, css, react and
-                                            bootstrap
-                                        </p>
-                                        <a href="https://github.com/mangeshii/portfolio">
-                                            <button className="gitbutton">
-                                                See on Github{" "}
-                                            </button>
-                                        </a>
-                                    </div>
+                        <div class="flip-card col-lg-4 col-md-6 col-xs-12">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front card3">
+                                    <img
+                                        className="frontcard"
+                                        src={pokedex}
+                                        alt="Avatar"
+                                    />
+                                    <h3>POKEDEX</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <p>
+                                        It consists of all the pokemon data
+                                        which is fetched from API. Once you
+                                        click in a particular pokemon card it
+                                        with render a page will more information
+                                        about that pokemon.
+                                    </p>
+                                    <a href="https://github.com/mangeshii/pokedex-using-react">
+                                        <button className="gitbutton">
+                                            See on Github
+                                        </button>
+                                    </a>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="col">
-                            <div class="flip-card ">
-                                <div class="flip-card-inner">
-                                    <div class="flip-card-front card5">
-                                        <img
-                                            className="frontcard"
-                                            src={randomquote}
-                                            alt="Avatar"
-                                        />
-                                        <h3>RANDOM QUOTE GENERATOR</h3>
-                                    </div>
-                                    <div class="flip-card-back">
-                                        <p>
-                                            A Random Quote Generator is capable
-                                            of pulling quotes randomly from an
-                                            API. It is also capable of tweeting
-                                            one of the random quote to the
-                                            twitter
-                                        </p>
-                                        <a href="https://github.com/mangeshii/random-quote-generator">
-                                            <button className="gitbutton">
-                                                See on Github
-                                            </button>
-                                        </a>
-                                    </div>
+                        <div class="flip-card col-lg-4 col-md-6 col-xs-12">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front card4">
+                                    <img
+                                        className="frontcard"
+                                        src={portfolio}
+                                        alt="Avatar"
+                                    />
+                                    <h3>PORTFOLIO</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <p>
+                                        It is a responsive website made with the
+                                        help of html, css, react and bootstrap
+                                    </p>
+                                    <a href="https://github.com/mangeshii/portfolio">
+                                        <button className="gitbutton">
+                                            See on Github
+                                        </button>
+                                    </a>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="col">
-                            <div class="flip-card">
-                                <div class="flip-card-inner">
-                                    <div class="flip-card-front card6">
-                                        <img style={{opacity:0.5}}
-                                            className="frontcard"
-                                            src={landingpage}
-                                            alt="Avatar"
-                                        />
-                                        <h3>LANDING PAGE</h3>
-                                    </div>
-                                    <div class="flip-card-back">
-                                        <p>
-                                                This is a responsive landing page.
-                                        </p>
-                                        <a href="https://github.com/mangeshii/landing-page">
-                                            <button className="gitbutton">
-                                                See on Github
-                                            </button>
-                                        </a>
-                                    </div>
+                        <div class="flip-card col-lg-4 col-md-6 col-xs-12">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front card5">
+                                    <img
+                                        className="frontcard"
+                                        src={randomquote}
+                                        alt="Avatar"
+                                    />
+                                    <h3>RANDOM QUOTE GENERATOR</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <p>
+                                        A Random Quote Generator is capable of
+                                        pulling quotes randomly from an API. It
+                                        is also capable of tweeting one of the
+                                        random quote to the twitter
+                                    </p>
+                                    <a href="https://github.com/mangeshii/random-quote-generator">
+                                        <button className="gitbutton">
+                                            See on Github
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="flip-card col-lg-4 col-md-6 col-xs-12">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front card6">
+                                    <img
+                                        className="frontcard"
+                                        src={landingpage}
+                                        alt="Avatar"
+                                    />
+                                    <h3>LANDING PAGE</h3>
+                                </div>
+                                <div class="flip-card-back">
+                                    <p>This is a responsive landing page.</p>
+                                    <a href="https://github.com/mangeshii/landing-page">
+                                        <button className="gitbutton">
+                                            See on Github
+                                        </button>
+                                    </a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
                 <div className="github-link-btn">
                     <a href="https://github.com/mangeshii?tab=repositories">
-                        <button className="github-link">See more on Github</button>
+                        <button className="github-link">
+                            See more on Github
+                        </button>
                     </a>
                 </div>
             </section>
